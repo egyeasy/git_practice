a15c96da885600a206aca72ebc58a999d94a28a6
egyeasy <dz1120@gmail.com>
2019-01-14 11:17:33 +0900

    1901114

--- browser.py
+++ browser.py
@@ -1,16 +1,16 @@
 import webbrowser
 
 # 모모랜드 모든 멤버들의 검색 페이지를 한번에 여는 코드
-keyword = input("검색어를 입력해 주세요: ")
+# keyword = input("검색어를 입력해 주세요: ")
 
 url = "https://search.daum.net/search?q="
 
-webbrowser.open()
+# webbrowser.open(url)
 
-# momo = ["나윤", "혜빈", "아인", "낸시", "주이",
-#     "연우", "제인", "데이지", "태하"]
+momo = ["나윤", "혜빈", "아인", "낸시", "주이",
+    "연우", "제인", "데이지", "태하"]
 
-# #momo라고 하는 리스트를 한번씩 돌면서 웹브라우저를 연다.
+#momo라고 하는 리스트를 한번씩 돌면서 웹브라우저를 연다.
 
-# for member in momo:
-#     webbrowser.open(url + member)
+for member in momo:
+    webbrowser.open(url + member)
